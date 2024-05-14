eda3b9ea63be87fbfcd12b71ad57b636c490aa15
qasim12343 <qasem.yousifi20@gmail.com>
2024-05-14 23:10:47 +0330

    added uncomplete chain block cipher code

---
+++ CBC.py
@@ -0,0 +1,33 @@
+import BlockCipher as bc
+import random as rd
+
+
+class CBC:
+
+    def __init__(self, plainText, key):
+        self.plainText = plainText
+        self.key = key
+
+    def generate_subPlains64(self, plainText):
+        i = 0
+        subPlains_64 = []
+        if len(plainText) < 64:
+            return subPlains_64.append(plainText.zfill(64))
+
+        while True:
+            subPlains_64.append(plainText[i:i+64])
+            i += 64
+            if i >= len(plainText):
+                subPlains_64.append(plainText[i:len(plainText)].zfill(64))
+                return
+
+    def cellOps(self):
+
+        rand = rd.randint(1, 1000000000)
+        bin_rand = bin(rand)[2:]
+        bin_rand = bin_rand.zfill(64)
+
+        bc.xor(self.plainText, self.bin_rand)
+
+
+c = CBC()
